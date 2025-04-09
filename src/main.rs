b45b3659d8e1f77a15a3dbfa5a9f1dddd9de3126
Evyn Rissling <evyn710@gmail.com>
2025-04-08 23:57:06 -0600

    Added some threading to the implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use iced::widget::{canvas, container};
+use iced::widget::canvas;
 use iced::{mouse, Color, Element, Fill, Point, Rectangle, Renderer, Size, Theme};
 
 use num::complex::Complex;
 
-use std::time::{Duration, Instant};
+use std::sync::mpsc::channel;
+use std::time::Instant;
+
+use threadpool::ThreadPool;
 
 struct Pixel {
     x: f32,
@@ -21,10 +24,13 @@ struct Mandelbrot {}
 
 impl Mandelbrot {
     fn view(&self) -> Element<Message> {
-        canvas(MandelbrotDrawing { pixels: Vec::new() })
-            .width(Fill)
-            .height(Fill)
-            .into()
+        println!("Hello");
+        canvas(MandelbrotDrawing {
+            threadpool: ThreadPool::new(12),
+        })
+        .width(Fill)
+        .height(Fill)
+        .into()
     }
 
     fn update(&mut self, message: Message) {}
@@ -32,37 +38,58 @@ impl Mandelbrot {
 
 #[derive(Default)]
 struct MandelbrotDrawing {
-    pixels: Vec<Pixel>,
+    threadpool: ThreadPool,
 }
 
-fn threaded_fractal_calc(start_column: i32, end_column: i32, bounds: &Rectangle) -> Vec<Pixel> {
-    let mut result: Vec<Pixel> = Vec::new();
-    for x in start_column..end_column as i32 {
-        for y in 0..bounds.height as i32 {
-            let i = (x as f32 - bounds.width * 2.0 / 3.0) / (bounds.width / 3.0);
-            let j = (y as f32 - bounds.height / 2.0) / (bounds.width / 3.0);
-            let c = Complex::new(i, j);
-            let mut z = Complex::new(0.0, 0.0);
-            let mut diverged = false;
-            for _ in 0..100 {
-                z = z * z + c;
-                if !z.is_finite() {
-                    diverged = true;
-                    break;
+fn threaded_fractal_calc(pool: &ThreadPool, bounds: Rectangle) -> Vec<Pixel> {
+    let mut overall_result: Vec<Pixel> = Vec::new();
+
+    let n_threads = pool.max_count();
+    let n_jobs = 40;
+
+    let pixel_thread_width = bounds.width / n_threads as f32;
+
+    let (tx, rx) = channel();
+    for i in 0..n_jobs {
+        let tx = tx.clone();
+        let start_column = i * pixel_thread_width as usize;
+        let end_column = start_column + pixel_thread_width as usize;
+        pool.execute(move || {
+            let mut result: Vec<Pixel> = Vec::new();
+            for x in start_column..end_column {
+                for y in 0..bounds.height as usize {
+                    let i = (x as f32 - bounds.width * 2.0 / 3.0) / (bounds.width / 3.0);
+                    let j = (y as f32 - bounds.height / 2.0) / (bounds.width / 3.0);
+                    let c = Complex::new(i, j);
+                    let mut z = Complex::new(0.0, 0.0);
+                    let mut diverged = false;
+                    for _ in 0..100 {
+                        z = z * z + c;
+                        if !z.is_finite() {
+                            diverged = true;
+                            break;
+                        }
+                    }
+
+                    if !diverged {
+                        result.push(Pixel {
+                            x: x as f32,
+                            y: y as f32,
+                            color: Color::BLACK,
+                        });
+                    }
                 }
             }
-
-            if !diverged {
-                result.push(Pixel {
-                    x: x as f32,
-                    y: y as f32,
-                    color: Color::BLACK,
-                });
-            }
-        }
+            tx.send(result)
+                .expect("channel will be there waiting for the result");
+        });
     }
 
-    result
+    for _ in 0..n_jobs {
+        overall_result.append(&mut rx.recv().unwrap());
+    }
+
+    overall_result
 }
 
 impl<Message> canvas::Program<Message> for MandelbrotDrawing {
@@ -81,7 +108,7 @@ impl<Message> canvas::Program<Message> for MandelbrotDrawing {
         let start = Instant::now();
         let background = canvas::Path::rectangle(Point::new(0.0, 0.0), bounds.size());
         frame.fill(&background, Color::WHITE);
-        let result = threaded_fractal_calc(0, bounds.width as i32, &bounds);
+        let result = threaded_fractal_calc(&self.threadpool, bounds);
         for pixel in result {
             let pixel_rectangle =
                 canvas::Path::rectangle(Point::new(pixel.x, pixel.y), Size::new(1.0, 1.0));
