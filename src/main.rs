bc3a8047604037edb7a3cbffbd03edf1bc4daff1
Evyn Rissling <evyn710@gmail.com>
2025-04-08 23:15:52 -0600

    Optimized drawing of fractal

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use iced::{Color, Element, Renderer, Theme, Rectangle, Point, Size, Fill, mouse};
-use iced::widget::{container, canvas};
+use iced::widget::{canvas, container};
+use iced::{mouse, Color, Element, Fill, Point, Rectangle, Renderer, Size, Theme};
 
 use num::complex::Complex;
+
+use std::time::{Duration, Instant};
 
 struct Pixel {
     x: f32,
@@ -15,16 +17,17 @@ enum Message {
 }
 
 #[derive(Default)]
-struct Mandelbrot {
-}
+struct Mandelbrot {}
 
 impl Mandelbrot {
     fn view(&self) -> Element<Message> {
-        canvas(MandelbrotDrawing{ pixels: Vec::new() }).width(Fill).height(Fill).into()
+        canvas(MandelbrotDrawing { pixels: Vec::new() })
+            .width(Fill)
+            .height(Fill)
+            .into()
     }
 
-    fn update(&mut self, message: Message) {
-    }
+    fn update(&mut self, message: Message) {}
 }
 
 #[derive(Default)]
@@ -41,29 +44,37 @@ impl<Message> canvas::Program<Message> for MandelbrotDrawing {
         renderer: &Renderer,
         _theme: &Theme,
         bounds: Rectangle,
-        _cursor: mouse::Cursor
+        _cursor: mouse::Cursor,
     ) -> Vec<canvas::Geometry> {
         let mut frame = canvas::Frame::new(renderer, bounds.size());
         println!("{:#?}", bounds.size());
+        let start = Instant::now();
+        let background = canvas::Path::rectangle(Point::new(0.0, 0.0), bounds.size());
+        frame.fill(&background, Color::WHITE);
         for x in 0..bounds.size().width as i32 {
             for y in 0..bounds.size().height as i32 {
-                let rectangle = canvas::Path::rectangle(Point::new(x as f32, y as f32), Size::new(1.0, 1.0));
-                let mut color = Color::BLACK;
+                let rectangle =
+                    canvas::Path::rectangle(Point::new(x as f32, y as f32), Size::new(1.0, 1.0));
                 let i = (x as f32 - bounds.width * 2.0 / 3.0) / (bounds.width / 3.0);
                 let j = (y as f32 - bounds.height / 2.0) / (bounds.width / 3.0);
                 let c = Complex::new(i, j);
                 let mut z = Complex::new(0.0, 0.0);
-                for n in 0..100 {
+                let mut diverged = false;
+                for _ in 0..100 {
                     z = z * z + c;
                     if !z.is_finite() {
-                        color = Color::WHITE;
+                        diverged = true;
                         break;
                     }
                 }
-                frame.fill(&rectangle, color);
 
+                if !diverged {
+                    frame.fill(&rectangle, Color::BLACK);
+                }
             }
         }
+        let duration = start.elapsed();
+        println!("{:#?}", duration);
 
         vec![frame.into_geometry()]
     }
